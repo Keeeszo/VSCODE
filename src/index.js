e5543387f78d12c0f0e1578541f696c257ac6ea3
Keszo Romeo <oscarromeo11@hotmail.com>
2019-07-30 03:55:52 -0500

    Se agregan las dependencias.

---
+++ src/index.js
@@ -0,0 +1,16 @@
+const express = require("express");
+const app = express();
+
+//Configuraciones 
+app.set('port', process.env.PORT || 3000);
+
+//Middlewares
+app.use(express.json());
+
+//Rutas
+app.use(require('./rutas/empleados'));
+
+//Empezando el servidor
+app.listen(app.get('port'), () => {
+  console.log("Server on port", app.get('port'));
+});
